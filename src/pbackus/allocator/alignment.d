7530936e971176ff773b5a4f117bee2874fabeeb
Paul Backus <snarwin@gmail.com>
2023-10-28 10:42:15 -0400

    Add basic alignment utilities

---
+++ src/pbackus/allocator/alignment.d
@@ -0,0 +1,36 @@
+module pbackus.allocator.alignment;
+
+import std.algorithm.comparison: max;
+import std.math.traits: isPowerOf2;
+
+enum platformAlignment = max(double.alignof, real.alignof);
+
+static assert(platformAlignment.isPowerOf2);
+
+@safe pure nothrow @nogc
+size_t roundToAligned(size_t size)
+{
+	size_t rem = size % platformAlignment;
+	return rem ? size + (platformAlignment - rem) : size;
+}
+
+@safe unittest
+{
+	assert(roundToAligned(0) == 0);
+}
+
+@safe unittest
+{
+	enum biggestAlloc = size_t.max & (platformAlignment - 1);
+	size_t[] sizes = [1, 7, 8, 9, 15, 16, 17, biggestAlloc - 1, biggestAlloc];
+
+	foreach (size; sizes)
+	{
+		size_t rounded = roundToAligned(size);
+
+		assert(rounded % platformAlignment == 0);
+		assert(rounded >= size);
+		assert(rounded >= platformAlignment);
+		assert(rounded - platformAlignment < size);
+	}
+}
