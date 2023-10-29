2f8a3b60931a343157d00af03ae6b47099295a82
Paul Backus <snarwin@gmail.com>
2023-10-29 14:24:08 -0400

    Rename FixedBuffer -> InSituRegion
    Easier to compare to std.experimental.allocator if the names are all the
    same.

--- src/pbackus/allocator/region.d
+++ src/pbackus/allocator/region.d
@@ -1,9 +1,9 @@
-module pbackus.allocator.fixed_buffer;
+module pbackus.allocator.region;
 
 import pbackus.allocator.alignment;
 import pbackus.allocator.block;
 
-struct FixedBuffer(size_t bufferSize)
+struct InSituRegion(size_t bufferSize)
 {
 	private @system {
 		align(platformAlignment) void[bufferSize] storage;
@@ -11,24 +11,24 @@ struct FixedBuffer(size_t bufferSize)
 	}
 
 	@trusted pure nothrow @nogc
-	Block!FixedBuffer allocate(size_t size)
+	Block!InSituRegion allocate(size_t size)
 	{
 		import core.lifetime: move;
 
 		if (size == 0 || size > maxAlignedSize)
-			return Block!FixedBuffer.init;
+			return Block!InSituRegion.init;
 
 		size_t roundedSize = roundToAligned(size);
 		if (roundedSize > bufferSize - inUse)
-			return Block!FixedBuffer.init;
+			return Block!InSituRegion.init;
 
-		Block!FixedBuffer result = storage[inUse .. inUse + roundedSize];
+		Block!InSituRegion result = storage[inUse .. inUse + roundedSize];
 		inUse += roundedSize;
 		return move(result);
 	}
 
 	@trusted pure nothrow @nogc
-	bool owns(ref const Block!FixedBuffer block) const
+	bool owns(ref const Block!InSituRegion block) const
 	{
 		return !block.isNull
 			&& &block.memory[0] >= &storage[0]
@@ -36,7 +36,7 @@ struct FixedBuffer(size_t bufferSize)
 	}
 
 	@trusted pure nothrow @nogc
-	void deallocate(ref Block!FixedBuffer block)
+	void deallocate(ref Block!InSituRegion block)
 	{
 		if (block.isNull)
 			return;
@@ -48,7 +48,7 @@ struct FixedBuffer(size_t bufferSize)
 		if (blockOffset + block.size == inUse)
 		{
 			inUse -= block.size;
-			block = Block!FixedBuffer.init;
+			block = Block!InSituRegion.init;
 		}
 	}
 }
@@ -56,7 +56,7 @@ struct FixedBuffer(size_t bufferSize)
 // Allocates blocks of the correct size
 @safe unittest
 {
-	FixedBuffer!128 buf;
+	InSituRegion!128 buf;
 	auto block = buf.allocate(32);
 	assert(!block.isNull);
 	assert(block.size >= 32);
@@ -65,7 +65,7 @@ struct FixedBuffer(size_t bufferSize)
 // Can't over-allocate
 @safe unittest
 {
-	FixedBuffer!128 buf;
+	InSituRegion!128 buf;
 	auto block = buf.allocate(256);
 	assert(block.isNull);
 }
@@ -73,7 +73,7 @@ struct FixedBuffer(size_t bufferSize)
 // Can't allocate when full
 @safe unittest
 {
-	FixedBuffer!128 buf;
+	InSituRegion!128 buf;
 	auto b1 = buf.allocate(128);
 	auto b2 = buf.allocate(1);
 	assert(!b1.isNull);
@@ -83,10 +83,10 @@ struct FixedBuffer(size_t bufferSize)
 // owns
 @safe unittest
 {
-	FixedBuffer!128 buf1, buf2;
+	InSituRegion!128 buf1, buf2;
 	auto b1 = buf1.allocate(32);
 	auto b2 = buf2.allocate(32);
-	auto b3 = Block!(FixedBuffer!128).init;
+	auto b3 = Block!(InSituRegion!128).init;
 
 	assert(buf1.owns(b1));
 	assert(!buf1.owns(b2));
@@ -100,7 +100,7 @@ struct FixedBuffer(size_t bufferSize)
 // Can deallocate an allocated block
 @safe unittest
 {
-	FixedBuffer!128 buf;
+	InSituRegion!128 buf;
 	auto block = buf.allocate(32);
 	buf.deallocate(block);
 	assert(block.isNull);
@@ -109,7 +109,7 @@ struct FixedBuffer(size_t bufferSize)
 // Deallocated space can be allocated again
 @safe unittest
 {
-	FixedBuffer!128 buf;
+	InSituRegion!128 buf;
 	auto block = buf.allocate(128);
 	buf.deallocate(block);
 	block = buf.allocate(32);
@@ -119,7 +119,7 @@ struct FixedBuffer(size_t bufferSize)
 // Can only deallocate the most recently allocated block
 @safe unittest
 {
-	FixedBuffer!128 buf;
+	InSituRegion!128 buf;
 	auto b1 = buf.allocate(32);
 	auto b2 = buf.allocate(32);
 	// should fail
