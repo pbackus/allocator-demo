b9e4de03aa1de4dc68580a0c8310f1484a2f40ba
Paul Backus <snarwin@gmail.com>
2023-11-10 11:22:06 -0500

    Add documentation for alignment module

--- src/pbackus/allocator/alignment.d
+++ src/pbackus/allocator/alignment.d
@@ -1,8 +1,15 @@
+/++
+Alignment-related utilities
+
+License: Boost License 1.0
+Authors: Paul Backus
++/
 module pbackus.allocator.alignment;
 
 import std.algorithm.comparison: max;
 import std.math.traits: isPowerOf2;
 
+/// Alignment sufficient for any type without an `align` attribute
 enum platformAlignment = max(double.alignof, real.alignof);
 
 @safe unittest
@@ -10,6 +17,7 @@ enum platformAlignment = max(double.alignof, real.alignof);
 	static assert(platformAlignment.isPowerOf2);
 }
 
+/// Largest `size_t` that's a multiple of `platformAlignment`
 enum maxAlignedSize = size_t.max & ~(platformAlignment - 1);
 
 @safe unittest
@@ -17,6 +25,7 @@ enum maxAlignedSize = size_t.max & ~(platformAlignment - 1);
 	static assert(maxAlignedSize % platformAlignment == 0);
 }
 
+/// Rounds `size` up to a multiple of `platformAlignment`, if necessary
 @safe pure nothrow @nogc
 size_t roundToAligned(size_t size)
 {
