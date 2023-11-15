eb669f0379a89a7695fdf7117d3211b07fed2d23
Paul Backus <snarwin@gmail.com>
2023-11-14 23:24:13 -0500

    util: add forceInferSystem

--- src/pbackus/util.d
+++ src/pbackus/util.d
@@ -17,3 +17,21 @@ enum trusted(string expr) = "(auto ref () @trusted => " ~ expr ~ ")()";
 	assert(mixin(trusted!"n") == 123);
 	assert(mixin(trusted!"2*n + 1") == 247);
 }
+
+/// A function that calls this will be inferred as `@system`
+pure nothrow @nogc
+void forceInferSystem() {}
+
+// hasFunctionAttributes uses druntime features in CTFE
+version (D_BetterC) {} else
+@safe unittest
+{
+	import std.traits: hasFunctionAttributes;
+
+	static void test()
+	{
+		forceInferSystem;
+	}
+
+	static assert(hasFunctionAttributes!(test, "@system"));
+}
